094b4cf43599b77f4c18310a35788ad7a235eb97
Naiman <85957227+NaimanMpl@users.noreply.github.com>
2024-06-04 02:13:33 +0200

    update: updated evyl description + new fav icon

--- src/pages/works/EvylWork.tsx
+++ src/pages/works/EvylWork.tsx
@@ -17,7 +17,7 @@ const EvylWork = () => {
         animate={{ opacity: 1, transition: { delay: .2 }}} 
         className='px-header py-12 mt-44 gap-20 flex justify-between'
       >
-        <h2 className='text-4xl font-medium max-w-5xl'>Born to be the <span className='text-green-550'>Game Of The Year</span> forced to be a school project</h2>
+        <h2 className='text-4xl font-medium max-w-5xl'>Born to be the <span className='text-green-550'>Game Of The Year</span> forced to be in the Hall Of Fame</h2>
         <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit veniam quia enim esse dolor aspernatur soluta eius possimus, nam commodi!</p>
       </motion.section>
     </Work>
